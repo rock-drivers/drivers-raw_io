ec6407f6d6cd65d73d07f4daa08e9940a8ae025f
RockStar <rockstar@rockstar-Inspiron-580.(none)>
2014-05-31 14:35:44 -0300

    initial commit2.

--- test/suite.cpp
+++
@@ -1,5 +0,0 @@
-// Do NOT add anything to this file
-// This header from boost takes ages to compile, so we make sure it is compiled
-// only once (her)
-#define BOOST_TEST_MODULE raw_io
-#include <boost/test/included/unit_test.hpp>
