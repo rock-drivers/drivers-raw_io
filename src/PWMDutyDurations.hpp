15fafeea33ea71d7e58ffbb679e4aaee6f81857f
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2025-09-03 17:27:58 -0300

    feat: representation for duration of the duty part of a PWM cycle
    Generally useful for servos and ESCs

---
+++ src/PWMDutyDurations.hpp
@@ -0,0 +1,18 @@
+#ifndef RAW_IO_PWMDUTYDURATIONS_HPP
+#define RAW_IO_PWMDUTYDURATIONS_HPP
+
+#include <cstdint>
+#include <base/Time.hpp>
+
+namespace raw_io {
+    /** Representation of a PWM command */
+    struct PWMDutyDurations {
+        /** @meta role logical_time */
+        base::Time timestamp;
+
+        /** Duration of the ON part of the cycle, in nanoseconds */
+        std::vector<std::uint32_t> on_durations;
+    };
+}
+
+#endif
